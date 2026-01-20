7cb12f1c0f0a2b78c135f2c4fe24cf0769e12c9a
Oluwaseyi-vibex <oluseyiwmwm@gmail.com>
2026-01-21 00:07:17 +0100

    feat: cleaned the app.ts and added credentail: true to cors

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,10 @@
 import "express-async-errors";
-import express, { Request, Response } from "express";
+import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import bodyParser from "body-parser";
-import response from "./utils/responseObject";
 
 import errorMiddleware from "./middlewares/error.middleware";
-import statusCodes from "./constants/statusCodes";
 import { apiLimiter } from "./middlewares/rateLimit.middleware";
 import rootRouter from "./routes";
 
@@ -15,35 +13,25 @@ const app = express();
 app.use(
   cors({
     origin: "http://localhost:3000",
+    credentials: true,
   })
 );
+
 app.use(bodyParser.json());
 app.use(apiLimiter);
+
 app.use(
   morgan("combined", {
-    skip: (req: Request, res: Response) => {
-      return res.statusCode < 400;
-    },
+    skip: (_req, res) => res.statusCode < 400,
   })
 );
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin,X-Requested-With, Content-Type, Accept"
-  );
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "PUT,POST, PATCH, DELETE, GET");
-    return res.status(200).json({});
-  }
-  next();
-});
 
-app.get("/", (req, res) => {
+app.get("/", (_req, res) => {
   res.send("Happy Whale");
 });
 
 app.use("/api/v1", rootRouter);
 
 app.use(errorMiddleware);
+
 export default app;
